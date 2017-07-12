ece6edb072636551eb241a5afdb1a0e794d6ee16
gaohang <2576865876@qq.com>
2017-07-12 10:12:42 +0800

    create the jni project with cmake

---
+++ app/jni/jni_lib.h
@@ -0,0 +1,12 @@
+//
+// Created by gaohang on 17/7/12.
+//
+
+#ifndef JNITEST_JNI_LIB_H
+#define JNITEST_JNI_LIB_H
+#include "jni.h"
+jstring GetStrFromJNI(JNIEnv* env,jobject callObj);
+
+#endif //JNITEST_JNI_LIB_H
+
+
---
+++ app/src/main/java/com/tigether/jnitest/MainActivity.java
@@ -0,0 +1,33 @@
+package com.tigether.jnitest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    static {
+        /*
+        加载动态库，动态库加载的时候 JNI_OnLoad函数会被调用
+
+        在JNI——OnLoad函数中，Java虚拟机通过函数表的形式将JNI函数和java类中native函数对应起来
+         */
+        System.loadLibrary("native-lib");
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, GetStrFromJNI(), Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+    Jni 函数的声明
+    当调用到此函数时，java虚拟机会通过JNI_OnLoad里注册的函数表找到对应的函数去执行
+     */
+    private native String GetStrFromJNI();
+
+}
